8e37a053ab479e610c2084ee320dbddbe2e4c4e6
chaeyeon-yang <myday0827@naver.com>
2024-04-29 16:01:33 +0900

    🎉[feat]: initial commit

---
+++ src/test/java/com/fromits/FromitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fromits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
