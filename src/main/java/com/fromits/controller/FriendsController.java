3adc0a27390988b7b2ad628f24f95e4e6a955591
allzeroyou <ekdudd119@naver.com>
2024-05-02 17:51:39 +0900

    [feat]: 친구 검색 백엔드 기능 개발 중

--- src/main/java/com/fromits/controller/FriendsController.java
+++ src/main/java/com/fromits/controller/FriendsController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
@@ -18,13 +22,24 @@ public class FriendsController {
   final CustService custService;
 
   @RequestMapping("/search")
-  public String search(Model model) throws Exception {
-    model.addAttribute("center",dir+"search");
+  public String searchByTitle(Model model, @RequestParam("query") String query, @RequestParam("key") String key) throws Exception {
+
+    Map<String, Object> paramMap = new HashMap<>();
+    paramMap.put("searchKeyword", query);
+
+    List<CustDto> list = null;
+
+    try {
+      if ("id".equals(key)) {
+        list = custService.findInfoById(paramMap);
+      }
+      model.addAttribute("list2", list);
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+    model.addAttribute("center", dir + "search");
     return "main";
   }
-
-
-
-
 }
 
