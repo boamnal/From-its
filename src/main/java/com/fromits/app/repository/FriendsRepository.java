1feffc25cb1f64eae49af19184eef11baeac0517
allzeroyou <ekdudd119@naver.com>
2024-05-03 16:56:20 +0900

    [feat]: 친구 추가 백엔드 기능 개발

--- src/main/java/com/fromits/app/repository/FriendsRepository.java
+++ src/main/java/com/fromits/app/repository/FriendsRepository.java
@@ -1,6 +1,7 @@
 package com.fromits.app.repository;
 
 import com.fromits.app.dto.CustDto;
+import com.fromits.app.dto.FriendsDto;
 import com.fromits.app.frame.HanaRepository;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,8 @@ import java.util.Map;
 
 @Repository
 @Mapper
-public interface FriendsRepository extends HanaRepository<String, CustDto> {
+public interface FriendsRepository extends HanaRepository<String, FriendsDto> {
+
+  void addFriend(FriendsDto dto);
 
 }
